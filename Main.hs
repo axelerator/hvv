0aa5f9bb0300642026191c2eab8c4d44518e01f3
Axel Tetzlaff <axel.tetzlaff@gmx.de>
2011-03-14 00:07:21 +0100

    added load & export of data

--- Main.hs
+++ Main.hs
@@ -5,11 +5,12 @@ import Data.Binary
 import Data.Binary.Get
 import Data.DateTime
 import Data.Word
+import System.IO
 
 import Data.Cli
 
 data HvvTariff = Kurz | Nah | Gross | Day9
-  deriving (Show, Eq, Ord)
+  deriving (Show, Eq, Ord, Enum)
 
 data Ticket = Ticket {
   t_day :: Int,
@@ -18,18 +19,11 @@ data Ticket = Ticket {
 } deriving (Eq, Show)
 
 instance Binary HvvTariff where
-  put Kurz = put (0::Word8)
-  put Nah = put (1::Word8)
-  put Gross = put (2::Word8)
-  put Day9 = put (3::Word8)
+  put t = put $ fromEnum t
 
   get = do
-    i <- getWord8
-    case i of
-      0 -> return Kurz
-      1 -> return Nah
-      2 -> return Gross
-      3 -> return Day9
+    i <- get
+    return $ toEnum i
   
 instance Binary Ticket where
   put (Ticket d t m) = put d >> put m  >> put t
@@ -53,35 +47,42 @@ instance ProgramState AppState where
 data MainMenu = MainMenu
 
 instance Program MainMenu AppState where
-  cmds _ = comds
+  cmds _ = map wrap comds
+
+wrap :: (String, (Action AppState, String))-> (String, (Action AppState, String))
+wrap (s, (action,d)) = (s, (prompt action, d))
 
 comds = [
       ("q", (quit, "Quit")),
       ("m", (setMonth, "<month> : set month")),
       ("+", (roll   1, "Roll day forward")),
       ("-", (roll (-1), "Roll day back")),
-      ("n", (newT Nah, "add new kurz ticket")),
-      ("k", (newT Kurz, "add new nah ticket")),
-      ("g", (newT Gross, "add new gross ticket")),
-      ("d", (newT Day9, "add new day ticket")),
+      ("1", (newT Nah, "add new kurz ticket")),
+      ("2", (newT Kurz, "add new nah ticket")),
+      ("3", (newT Gross, "add new gross ticket")),
+      ("4", (newT Day9, "add new day ticket")),
       ("h", (printHelp, "print this help")),
+      ("e", (export, "<filename> export to file")),
       ("ls", (ls, "show tickets")),
-      ("s", (save, "Save to file"))
+      ("l", (load, "<filename> load from file")),
+      ("s", (save, "<filename> Save to file"))
     ]
 
 ls _ as = do
   mapM (putStrLn . printTicket) (tickets as)
   return as
 
+export filename state = do
+  mapM (putStrLn . exportTicket) (tickets state) 
+  return state
+
 
 setMonth ps (AppState ts f day _) = do
   let newMonth = read ps
-  putStrLn $ "Month is now: " ++ (show newMonth)
   return $ AppState ts f day newMonth
 
 roll :: Int -> Action AppState
 roll ff _ (AppState ts f d m) = do
-    putStrLn $ show (d + ff)
     return $ AppState ts f (d + ff) m
 
 quit _ (AppState ts _ d m) = do
@@ -100,10 +101,16 @@ printWelcome _ s = do
   return s
 
 save :: Action AppState
-save _ state@(AppState ts _ 0 0) = do
-  encodeFile "hvv.data" ts
+save filename state = do
+  encodeFile filename (tickets state)
   putStrLn "saved."
   return state
+
+load :: Action AppState
+load filename (AppState _ f d m) = do
+  ts <- decodeFile filename 
+  putStrLn "load."
+  return $ AppState ts f d m
 
 newT tariff _ (AppState ts f day month) = do
   putStrLn $ "Hinzugefügt: " ++ (show day) ++ "." ++ (show month) ++ " " ++ (show tariff)
@@ -111,13 +118,26 @@ newT tariff _ (AppState ts f day month) = do
   where 
     newTicket = Ticket day month tariff 
 
-value Nah = "1.30"
-value Kurz = "1.80"
-value Gross = "2.80"
-value Day9 = "5.50"
+value Nah = "1,30"
+value Kurz = "1,80"
+value Gross = "2,80"
+value Day9 = "5,50"
+
+prompt action ps as@(AppState ts f d m )= do
+  newState@(AppState _ _ nd nm ) <- action ps as
+  putStr $ "### " ++ (zls nd) ++ "." ++ (zls nm) ++ " -> "
+  hFlush stdout
+  return newState
 
 printTicket (Ticket d m t ) = (show d) ++ "." ++ (show m) ++ ": " ++ (value t) ++ "€"
 
+exportTicket (Ticket d m t) = ";\"H\";" ++ (value t) ++ ";;8663;;;" ++ (zls d) ++ (zls m) ++ ";79903;;;;;\"HVV\""
+
+zls d
+      | d < 10 = "0" ++ (show d)
+      | otherwise = show d  
+
+
 main = do
-  runProg MainMenu (AppState [] False 0 0)
+  runProg MainMenu (AppState [] False 1 1)
   return ()
